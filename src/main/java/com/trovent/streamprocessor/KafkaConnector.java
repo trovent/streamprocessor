e0a73317e3ec91e6a165211805aff23aa7dc8f3a
Tobias Nieberg <t.nieberg@trovent.io>
2019-04-09 14:35:20 +0200

    Add skeletons for Kafka and Esper engine integration

---
+++ src/main/java/com/trovent/streamprocessor/KafkaConnector.java
@@ -0,0 +1,5 @@
+package com.trovent.streamprocessor;
+
+public class KafkaConnector {
+
+}
---
+++ src/main/java/com/trovent/streamprocessor/TSPEngine.java
@@ -0,0 +1,40 @@
+package com.trovent.streamprocessor;
+
+import java.util.HashMap;
+
+public class TSPEngine {
+	
+	HashMap<Integer, String>	statements;
+	
+	
+	TSPEngine() {
+		statements = new HashMap<Integer, String>();
+	}
+	
+	public void init() {
+		
+	}
+	
+	public int addEPLStatement(String statement) {
+		int id=0;
+		
+		return id;
+	}
+	
+	public void removeEPLStatement(int id) {
+		
+	}
+	
+	public void startEPLStatement(int id)
+	{
+		
+	}
+	
+	public void stopEPLStatement(int id) {
+		
+	}
+	
+	public void addEPLListener(int id) {
+		
+	}
+}

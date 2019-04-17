da258d33eaaac3531cb1a6ad0385c5f4dd336b9c
Tobias Nieberg <t.nieberg@trovent.io>
2019-04-17 16:56:34 +0200

    Extend skeleton of REST API for esper

--- src/main/java/com/trovent/streamprocessor/restapi/EsperService.java
+++ src/main/java/com/trovent/streamprocessor/restapi/EsperService.java
@@ -1,10 +1,17 @@
 package com.trovent.streamprocessor.restapi;
 
+import java.util.function.Consumer;
+
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+
+import org.glassfish.jersey.internal.util.Producer;
 
 import com.trovent.streamprocessor.esper.EplStatement;
 
@@ -18,7 +25,7 @@ public class EsperService {
 		return "Hello World!\n";
 	}
 
-	@GET
+	@POST
 	@Path("addEplStatement")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
@@ -31,4 +38,48 @@ public class EsperService {
 		// TODO: delete from engine and list
 		return;
 	}
+
+	// { "topic" : "mytopic", "eventname" : "myeventname" }
+	@POST
+	@Path("addConsumer")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public int addConsumer(Consumer consumer) {
+		int id = 0;
+
+		return id;
+	}
+
+	// { "topic" : "mytopic", "statementname" : "mystmtname" }
+	@POST
+	@Path("addProducer")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public int addProducer(Producer producer) {
+		int id = 0;
+
+		return id;
+	}
+
+	@DELETE
+	@Path("consumer/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void deleteConsumer(@QueryParam("id") int id) {
+
+	}
+
+	@DELETE
+	@Path("producer/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void deleteProducer(@QueryParam("id") int id) {
+
+	}
+
+	@POST
+	@Path("sendEvent")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void sendEvent(GenericEventType event) {
+		// event.data["fieldname"] = "value"
+	}
+
 }
---
+++ src/main/java/com/trovent/streamprocessor/restapi/GenericEventType.java
@@ -0,0 +1,8 @@
+package com.trovent.streamprocessor.restapi;
+
+import java.util.Map;
+
+public class GenericEventType {
+	public Map<String, Object> data;
+
+}

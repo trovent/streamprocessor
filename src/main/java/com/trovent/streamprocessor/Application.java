81baa6330e0183fc55b3634d80a59353307530cb
Tobias Nieberg <t.nieberg@trovent.io>
2019-04-26 22:32:15 +0200

    Integrate kafka usage into Application

--- src/main/java/com/trovent/streamprocessor/Application.java
+++ src/main/java/com/trovent/streamprocessor/Application.java
@@ -5,33 +5,38 @@ import java.io.File;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.trovent.streamprocessor.kafka.InputProcessor;
+import com.trovent.streamprocessor.kafka.KafkaManager;
 
 public class Application {
 
 	private Logger logger;
-	
+
+	private KafkaManager kafkaManager;
+
 	final private String defaultConfigFile = "app.properties";
+
 	/**
 	 * Initialise application
+	 * 
 	 * @param args
-	 */	
+	 */
 	private void init(String[] args) {
 
 		// TODO
 		/*
-		 * parse commandline parameters
-		 * open configfile, read settings
-		 * initialise logging component
-		 *  
+		 * parse commandline parameters open configfile, read settings initialise
+		 * logging component
+		 * 
 		 */
-		
+
 		this.logger = LogManager.getLogger();
 		this.logger.trace("entering init()");
-		
+
 		Configuration config = new Configuration();
 		config.parseArguments(args);
-		
-		if (config.getConfigfile()!=null) {
+
+		if (config.getConfigfile() != null) {
 			this.logger.debug("reading config from: {}", config.getConfigfile());
 			File f = new File(config.getConfigfile());
 			if (!f.exists()) {
@@ -39,25 +44,27 @@ public class Application {
 				// read default properties file
 				config.readConfigFile(defaultConfigFile);
 			}
-		}
-		else {
+		} else {
 			// read default properties file
 			config.readConfigFile(defaultConfigFile);
 		}
-		
+
+		this.logger.info("creating KafkaManager");
+		this.kafkaManager = new KafkaManager();
+
 		this.logger.trace("init() done");
 	}
-	
-	
+
 	private void run() {
 		this.logger.trace("entering run()");
 		this.logger.info("starting Trovent Stream Processor");
 		// TODO
 		/*
-		 * init and start esper engine
-		 * test connection to kafka
-		 * start application server
+		 * init and start esper engine test connection to kafka start application server
 		 */
+
+		testConsumer();
+
 		this.logger.info("shutting down...");
 		this.logger.trace("run() done");
 	}
@@ -66,5 +73,14 @@ public class Application {
 		Application app = new Application();
 		app.init(args);
 		app.run();
+	}
+
+	private void testConsumer() {
+
+		// InputProcessor input = new CSVInputProcessor("myevent");
+		InputProcessor input = new JSONInputProcessor("myevent");
+
+		this.kafkaManager.createConsumer("syslog", input);
+		// this.kafkaManager.createConsumer("netflow", input);
 	}
 }

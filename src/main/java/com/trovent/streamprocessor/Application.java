1d33d4a8e7982abde8d118e6dd711de469454ab3
Tobias Nieberg <t.nieberg@e2security.de>
2019-04-27 10:53:46 +0200

    Fixing build

--- src/main/java/com/trovent/streamprocessor/Application.java
+++ src/main/java/com/trovent/streamprocessor/Application.java
@@ -1,29 +1,25 @@
 package com.trovent.streamprocessor;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.trovent.streamprocessor.restapi.ApplicationServer;
 //import com.sun.tools.doclets.internal.toolkit.util.DocFinder.Input;
 import com.trovent.streamprocessor.kafka.InputProcessor;
 import com.trovent.streamprocessor.kafka.KafkaManager;
-
+import com.trovent.streamprocessor.restapi.ApplicationServer;
 
 public class Application {
 
 	private Logger logger;
 	private TSPEngine engine = new TSPEngine();
-	
 
 	private KafkaManager kafkaManager;
 
 	final private String defaultConfigFile = "app.properties";
-	
+
 	private Configuration config;
 
 	/**
@@ -42,11 +38,11 @@ public class Application {
 
 		this.logger = LogManager.getLogger();
 		this.logger.trace("entering init()");
-		
+
 		this.config = new Configuration();
 		this.config.parseArguments(args);
-		
-		if (config.getConfigfile()!=null) {
+
+		if (config.getConfigfile() != null) {
 			this.logger.debug("reading config from: {}", config.getConfigfile());
 			File f = new File(config.getConfigfile());
 			if (!f.exists()) {
@@ -68,40 +64,32 @@ public class Application {
 	private void run() {
 		this.logger.trace("entering run()");
 		this.logger.info("starting Trovent Stream Processor");
-		
-		
-		// TEST START
-		System.out.println("this is System here: starting TestRun");
-		try {
-			this.interactiveStatementEntry();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		// TEST END
-		
+
 		/*
 		 * init and start esper engine test connection to kafka start application server
 		 */
 
 		testConsumer();
 
-		
 		ApplicationServer server = new ApplicationServer(this.config);
 		server.start();
-		
-		System.out.println(String.format("Application started and is listening on port %d", this.config.getPort() ));
-  
-		System.in.read();
 
-		
+		System.out.println(String.format("Application started and is listening on port %d", this.config.getPort()));
+
+		try {
+			System.in.read();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		this.logger.info("shutting down...");
-		
+
 		server.stop();
 		this.logger.trace("run() done");
 	}
 
-	public static void main(String[] args)  throws IOException {
+	public static void main(String[] args) throws IOException {
 		Application app = new Application();
 		app.init(args);
 		app.run();

05c6158a447f164b13d69ebffed1e20613425006
Lukas Steinbrink <l.steinbrink@e2security.de>
2019-04-29 19:12:45 +0200

    added Methods and Tests
    added RemoveEPLSchema, hasSchema, hasStatement.
    basic Tests added, RemoveEPLSchema needs more work still

--- src/test/java/com/trovent/streamprocessor/test/esper/TestTSPEngine.java
+++ src/test/java/com/trovent/streamprocessor/test/esper/TestTSPEngine.java
@@ -20,7 +20,7 @@ public class TestTSPEngine extends TestCase {
 	public TestTSPEngine() {
 		super();
 	}
-	
+
 	protected void setUp() throws Exception {
 		super.setUp();
 
@@ -67,50 +67,50 @@ public class TestTSPEngine extends TestCase {
 
 		final String SCHEMANAME = "createmyschema";
 		final String STATEMENTNAME = "mystatement";
-		
+
 		engine.addEPLStatement("create map schema inputqueue as (name string, age int)", SCHEMANAME);
 		String result = engine.addEPLStatement("select name from inputqueue", STATEMENTNAME);
-		
+
 		assertEquals(STATEMENTNAME, result);
 	}
-	
+
 	@Test
 	public void testSendEPLEventMAP() {
 		String statement;
 		statement = "create map schema SomeMapEventSchema as (first_name string, numbers integer)";
 		engine.addEPLStatement(statement, "MapSchema");
-		
+
 		statement = "select first_name as First_Name from SomeMapEventSchema";
 		engine.addEPLStatement(statement, "MapStatement");
-		
+
 		Map<String, Object> mapData = new HashMap<String, Object>();
 		mapData.put("numbers", 42);
 		mapData.put("first_name", "Alice");
-		
+
 		engine.sendEPLEvent("SomeMapEventSchema", mapData);
 	}
-	
+
 	@Test
 	public void testSendEPLEventOBJECTARRAY() {
 		String statement;
 		statement = "create objectarray schema SomeArrayEventSchema as (first_name string, numbers integer)";
 		engine.addEPLStatement(statement, "ArraySchema");
-		
+
 		statement = "select first_name as First_Name from SomeArrayEventSchema";
 		engine.addEPLStatement(statement, "ArrayStatement");
-		
-		//testData as objectarray
+
+		// testData as objectarray
 		Object[] objArrayData = new Object[2];
 		objArrayData[0] = new String("Alice");
 		objArrayData[1] = new Integer(42);
-		
+
 		engine.sendEPLEvent("SomeArrayEventSchema", objArrayData);
 	}
 
 	@Test
-	public void testAddEPLSchemaAllowedEntries( ) {
-		Map<String, String> newEventType = new HashMap<String, String>(); 
-		
+	public void testAddEPLSchemaAllowedEntries() {
+		Map<String, String> newEventType = new HashMap<String, String>();
+
 		newEventType.put("myString", "string");
 		newEventType.put("myinteger", "integer");
 		newEventType.put("myinteger2", "int");
@@ -119,19 +119,83 @@ public class TestTSPEngine extends TestCase {
 		newEventType.put("myDouble", "double");
 		newEventType.put("myFloat", "float");
 		newEventType.put("myByte", "byte");
-		
+
 		engine.addEPLSchema("TestEventSchema", newEventType);
 	}
-	
+
 	@Test
-	public void testAddEPLSchemaStrangeEntries( ) {
-		Map<String, String> newEventType = new HashMap<String, String>(); 
-		
+	public void testAddEPLSchemaStrangeEntries() {
+		Map<String, String> newEventType = new HashMap<String, String>();
+
 		newEventType.put("myString", "wooords");
 		newEventType.put("foo", "bar");
-		
-		assertThrows(EPException.class,
-				() -> engine.addEPLSchema("TestEvent", newEventType));
+
+		assertThrows(EPException.class, () -> engine.addEPLSchema("TestEvent", newEventType));
 	}
-		
+
+	@Test
+	public void testHasSchemaForSchemaExists() {
+		this.testAddEPLSchemaAllowedEntries();
+		assertTrue(engine.hasSchema("TestEventSchema"));
+	}
+
+	@Test
+	public void testHasSchemaForSchemaExistsCreatedByAddEPLStatement() {
+		String statement;
+		String statementName = "StatementMadeSchema";
+		String eventTypeName = "eventName";
+		statement = "create objectarray schema " + eventTypeName + " as (first_name string, numbers integer)";
+		engine.addEPLStatement(statement, statementName);
+
+		assertFalse(engine.hasSchema(statementName));
+		assertFalse(engine.hasStatement(eventTypeName));
+		assertTrue(engine.hasSchema(eventTypeName));
+		assertTrue(engine.hasStatement(statementName));
+	}
+
+	@Test
+	public void testHasSchemaForSchemaDoesNotExist() {
+		this.testAddEPLSchemaAllowedEntries();
+		assertFalse(engine.hasSchema("Bielefeld"));
+	}
+
+	@Test
+	public void testRemoveEPLSchema() {
+		this.testAddEPLSchemaAllowedEntries();
+		assertTrue(engine.hasSchema("TestEventSchema"));
+		engine.removeEPLSchema("TestEventSchema");
+		assertFalse(engine.hasSchema("TestEventSchema"));
+	}
+
+	@Test
+	public void testRemoveEPLSchemaCreatedByAddEPLStatement() {
+		String statement;
+		String statementName = "StatementMadeSchema";
+		String eventTypeName = "eventName";
+		statement = "create objectarray schema " + eventTypeName + " as (first_name string, numbers integer)";
+		engine.addEPLStatement(statement, statementName);
+
+		assertTrue(engine.hasSchema(eventTypeName));
+		engine.removeEPLSchema(eventTypeName); // i know this fails
+		assertFalse(engine.hasSchema(eventTypeName));
+	}
+
+	@Test
+	public void testRemoveEPLSchemaCreatedByAddEPLStatementInTwoSteps() {
+		String statement;
+		String statementName = "StatementMadeSchema";
+		String eventTypeName = "eventName";
+		statement = "create objectarray schema " + eventTypeName + " as (first_name string, numbers integer)";
+		engine.addEPLStatement(statement, statementName);
+
+		assertTrue(engine.hasSchema(eventTypeName));
+		assertTrue(engine.hasStatement(statementName));
+		engine.removeEPLStatement(statementName);
+		// assertTrue(engine.hasSchema(eventTypeName));
+		assertFalse(engine.hasStatement(statementName));
+		assertThrows(EPException.class, () -> engine.removeEPLSchema(eventTypeName));
+		assertFalse(engine.hasStatement(statementName));
+		assertFalse(engine.hasSchema(eventTypeName));
+	}
+
 }
--- src/test/java/com/trovent/streamprocessor/test/esper/TestTSPEngineStatements.java
+++ src/test/java/com/trovent/streamprocessor/test/esper/TestTSPEngineStatements.java
@@ -18,18 +18,18 @@ public class TestTSPEngineStatements extends TestCase {
 	public TestTSPEngineStatements() {
 		super();
 	}
-	
+
 	protected void setUp() throws Exception {
 		super.setUp();
 
 		engine = new TSPEngine();
 		engine.init();
-		
-		//creates a 
+
+		// creates a
 		String statement;
 		statement = "create map schema SomeMapEventSchema as (first_name string, numbers integer)";
 		engine.addEPLStatement(statement, "MapSchema");
-		
+
 		statement = "select count(first_name) as cntFirst_Name from SomeMapEventSchema";
 		engine.addEPLStatement(statement, "MapStatement");
 	}
@@ -39,84 +39,100 @@ public class TestTSPEngineStatements extends TestCase {
 		engine.shutdown();
 		super.tearDown();
 	}
-	
+
 	@Test
 	public void testStartEPLStatement() {
 		engine.startEPLStatement("MapStatement");
 		assertTrue(engine.getEPServiceProvider().getEPAdministrator().getStatement("MapStatement").isStarted());
 	}
-	
+
 	@Test
 	public void testStartEPLStatementSCHEMA() {
 		engine.stopEPLStatement("MapSchema");
 		assertFalse(engine.getEPServiceProvider().getEPAdministrator().getStatement("MapSchema").isStarted());
-		
+
 		engine.startEPLStatement("MapSchema");
 		assertTrue(engine.getEPServiceProvider().getEPAdministrator().getStatement("MapSchema").isStarted());
 	}
-	
+
 	@Test
 	public void testStartEPLStatementForNonexistantStatement() {
-		assertThrows(EPException.class, 
-				()-> engine.startEPLStatement("Bielefeld"));
+		assertThrows(EPException.class, () -> engine.startEPLStatement("Bielefeld"));
 	}
-	
+
 	@Test
 	public void testStopEPLStatement() {
 		assertTrue(engine.getEPServiceProvider().getEPAdministrator().getStatement("MapStatement").isStarted());
 		engine.stopEPLStatement("MapStatement");
 		assertTrue(engine.getEPServiceProvider().getEPAdministrator().getStatement("MapStatement").isStopped());
 	}
-	
+
 	@Test
 	public void testStopEPLStatementSCHEMA() {
 		assertTrue(engine.getEPServiceProvider().getEPAdministrator().getStatement("MapSchema").isStarted());
 		engine.stopEPLStatement("MapSchema");
 		assertTrue(engine.getEPServiceProvider().getEPAdministrator().getStatement("MapSchema").isStopped());
 	}
-	
+
 	@Test
 	public void testStopEPLStatementForNonexistantStatement() {
-		assertThrows(EPException.class, 
-				()-> engine.stopEPLStatement("Bielefeld"));		
+		assertThrows(EPException.class, () -> engine.stopEPLStatement("Bielefeld"));
 	}
-	
-	@Test//TODO
+
+	@Test // TODO
 	public void testRemoveEPLStatement() {
 		engine.removeEPLStatement("MapStatement");
 	}
-	
-	@Test//TODO
+
+	@Test // TODO
 	public void testRemoveEPLStatementSCHEMA() {
 		engine.removeEPLStatement("MapSchema");
 	}
-	
+
 	@Test
 	public void testRemoveEPLStatementForNonexistantStatement() {
-		assertThrows(EPException.class, 
-				()-> engine.removeEPLStatement("Bielefeld"));
+		assertThrows(EPException.class, () -> engine.removeEPLStatement("Bielefeld"));
 	}
-	
+
 	@Test
 	public void testAddListenerToStatement() throws InterruptedException {
 		class MyListener implements UpdateListener {
 			public void update(EventBean[] newEvents, EventBean[] oldEvents) {
-				//does Nothing
+				// does Nothing
 			}
 		}
 		MyListener newListener = new MyListener();
 		engine.addListener("MapStatement", newListener);
 	}
-	
+
 	@Test
 	public void testAddListenerToNonexistantStatement() {
-		class MyListener implements UpdateListener {			
+		class MyListener implements UpdateListener {
 			public void update(EventBean[] newEvents, EventBean[] oldEvents) {
-				//does Nothing
+				// does Nothing
 			}
 		}
-		assertThrows(EPException.class, 
-				()-> engine.addListener("Bielefeld", new MyListener()));		
+		assertThrows(EPException.class, () -> engine.addListener("Bielefeld", new MyListener()));
 	}
-	
+
+	@Test
+	public void testHasStatementForStatementExists() {
+		String statement;
+		String statementName = "ArraySchema";
+		statement = "create objectarray schema SomeArrayEventSchema as (first_name string, numbers integer)";
+		engine.addEPLStatement(statement, statementName);
+
+		assertTrue(engine.hasStatement(statementName));
+	}
+
+	@Test
+	public void testHasStatementForStatementDoesNotExist() {
+		String statement;
+		String statementName = "ArraySchema";
+		statement = "create objectarray schema SomeArrayEventSchema as (first_name string, numbers integer)";
+		engine.addEPLStatement(statement, statementName);
+
+		assertTrue(engine.hasStatement("Bielefeld"));
+	}
+
 }

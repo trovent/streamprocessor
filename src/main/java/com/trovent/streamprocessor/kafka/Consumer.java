bc02322f3562b10203ea6853a7e4f14cc100fba8
Tobias Nieberg <t.nieberg@trovent.io>
2019-04-29 17:18:04 +0200

    Move kafka properties handling into KafkaManager

--- src/main/java/com/trovent/streamprocessor/kafka/Consumer.java
+++ src/main/java/com/trovent/streamprocessor/kafka/Consumer.java
@@ -23,19 +23,12 @@ public class Consumer implements Runnable {
 
 	private boolean isStopped = true;
 
-	public Consumer(String topic, InputProcessor input) {
+	public Consumer(Properties props, String topic, InputProcessor input) {
 
 		this.topic = topic;
 		this.input = input;
 		this.logger = LogManager.getLogger();
 
-		Properties props = new Properties();
-		props.put("bootstrap.servers", "localhost:29092");
-		props.put("group.id", "test");
-		props.put("enable.auto.commit", "true");
-		props.put("auto.commit.interval.ms", "1000");
-		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-		props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 		this.consumer = new KafkaConsumer<>(props);
 
 		List<String> topics = new ArrayList<>();

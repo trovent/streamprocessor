ba5d81fce7dfb3eb7287075711796db982e35bd3
Tobias Nieberg <t.nieberg@e2security.de>
2019-05-05 12:54:15 +0200

    Add javadoc comments

--- src/main/java/com/trovent/streamprocessor/esper/EplSchema.java
+++ src/main/java/com/trovent/streamprocessor/esper/EplSchema.java
@@ -1,5 +1,11 @@
 package com.trovent.streamprocessor.esper;
 
+/**
+ * Entity class for epl schema data
+ * 
+ * @author tobias nieberg
+ *
+ */
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,17 +13,33 @@ public class EplSchema {
 	public String name;
 	public Map<String, String> fields;
 
+	/**
+	 * Default constructor creating empty object
+	 */
 	public EplSchema() {
 		fields = new HashMap<String, String>();
 	}
 
+	/**
+	 * Extended constructor. Use given name as schema name.
+	 * 
+	 * @param name name to be set for schema
+	 */
 	public EplSchema(String name) {
 		this.name = name;
 		fields = new HashMap<String, String>();
 	}
 
-	public EplSchema add(String key, String value) {
-		this.fields.put(key, value);
+	/**
+	 * Add field given as key value pair to schema.
+	 * 
+	 * @param fieldName name of the field to be defined
+	 * @param fieldType type of the field given as string
+	 * @return The current EplSchema object is returned. Can be used to create a
+	 *         call chain of the form add().add().add()...
+	 */
+	public EplSchema add(String fieldName, String fieldType) {
+		this.fields.put(fieldName, fieldType);
 		return this;
 	}
 }

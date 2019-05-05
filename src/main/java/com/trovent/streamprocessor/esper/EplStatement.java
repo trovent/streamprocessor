dbfa85efd53b7a80be5c1d2b088226251653436d
Tobias Nieberg <t.nieberg@e2security.de>
2019-05-05 12:53:57 +0200

    Add javadoc comments

--- src/main/java/com/trovent/streamprocessor/esper/EplStatement.java
+++ src/main/java/com/trovent/streamprocessor/esper/EplStatement.java
@@ -1,16 +1,38 @@
 package com.trovent.streamprocessor.esper;
 
+/**
+ * Entity class for epl statement data
+ * 
+ * @author tobias nieberg
+ *
+ */
 public class EplStatement {
 	public String name;
 	public String expression;
 
+	/**
+	 * Default constructor of EplStatement
+	 */
 	public EplStatement() {
 	}
 
+	/**
+	 * Extended constructor of EplStatement. The given name is set as name of the
+	 * EplStatement.
+	 * 
+	 * @param name name to be used as name of EplStatement
+	 */
 	public EplStatement(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * Extended constructor of EplStatement. Given name and expression are used as
+	 * name and expression of the EplStatement.
+	 * 
+	 * @param name       name to be used as name of EplStatement
+	 * @param expression expression to be used as expression of EplStatement
+	 */
 	public EplStatement(String name, String expression) {
 		this.name = name;
 		this.expression = expression;

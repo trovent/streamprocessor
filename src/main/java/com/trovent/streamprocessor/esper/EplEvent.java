86ae6e2bc387518866e398d0300cb979553297c0
Tobias Nieberg <t.nieberg@trovent.io>
2019-05-09 17:21:49 +0200

    Extend class EplEvent

--- src/main/java/com/trovent/streamprocessor/esper/EplEvent.java
+++ src/main/java/com/trovent/streamprocessor/esper/EplEvent.java
@@ -5,4 +5,18 @@ import java.util.LinkedHashMap;
 public class EplEvent {
 	public String eventTypeName;
 	public LinkedHashMap<String, Object> data;
+
+	public EplEvent() {
+		this.data = new LinkedHashMap<String, Object>();
+	}
+
+	public EplEvent(String eventTypeName) {
+		this.data = new LinkedHashMap<String, Object>();
+		this.eventTypeName = eventTypeName;
+	}
+
+	public EplEvent add(String fieldName, Object data) {
+		this.data.put(fieldName, data);
+		return this;
+	}
 }

b35b837ab424e52c94d8bf18d74691c65b264dad
Hrvoje Zeljko (hrvoje.zeljko) <hrvoje.zeljko@e-ito.hr>
2019-05-14 14:09:39 +0200

    Script for batch mode

--- run_producer.sh
+++ run_producer.sh
@@ -1,7 +1,8 @@
 source ./kafka.conf
-
 TOPIC="$1"
 
 [ -z "$TOPIC" ] && echo "ERROR: Argument <topic> is missing" && exit 1
 
-docker exec -it $CONTAINER /opt/kafka/bin/kafka-console-producer.sh --topic $TOPIC --broker-list $BOOTSTRAP_SERVER
+[ -t 0 ] && OPT_TTY="-t" && echo "Type input for topic $TOPIC:"
+
+docker exec -i $OPT_TTY $CONTAINER /opt/kafka/bin/kafka-console-producer.sh --topic $TOPIC --broker-list $BOOTSTRAP_SERVER

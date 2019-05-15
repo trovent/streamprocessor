a6e368392c9565d7f50411fc90120a1bfc1f892b
Tobias Nieberg <t.nieberg@trovent.io>
2019-05-15 16:34:39 +0200

    Add default constructor - needed by jackson json converter

--- src/main/java/com/trovent/streamprocessor/restapi/ConsumerConnector.java
+++ src/main/java/com/trovent/streamprocessor/restapi/ConsumerConnector.java
@@ -4,6 +4,10 @@ public class ConsumerConnector {
 	public String topic;
 	public String schemaName;
 
+	ConsumerConnector() {
+
+	}
+
 	public ConsumerConnector(String topic, String schemaName) {
 		this.topic = topic;
 		this.schemaName = schemaName;
--- src/main/java/com/trovent/streamprocessor/restapi/ProducerConnector.java
+++ src/main/java/com/trovent/streamprocessor/restapi/ProducerConnector.java
@@ -4,6 +4,10 @@ public class ProducerConnector {
 	public String topic;
 	public String eplStatementName;
 
+	public ProducerConnector() {
+
+	}
+
 	public ProducerConnector(String topic, String eplStatementName) {
 		this.topic = topic;
 		this.eplStatementName = eplStatementName;
